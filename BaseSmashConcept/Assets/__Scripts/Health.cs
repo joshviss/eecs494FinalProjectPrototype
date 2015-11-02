d20c37ec8055b0d9d1c3b85cd76df402972f03d1
HwangN <nhwa@umich.edu>
2015-11-02 13:15:00 -0500

    Displays health - not that it changes much right now...

---
+++ BaseSmashConcept/Assets/__Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+	Text health;
+	public Player S;
+
+	// Use this for initialization
+	void Start () {
+		health = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShowText ();
+	}
+
+	void ShowText(){
+		health.text = "HP: " + S.GetHealth ();
+	}
+}
