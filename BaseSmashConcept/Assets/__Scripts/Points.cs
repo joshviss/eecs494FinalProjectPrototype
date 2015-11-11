2a94165776a8b42792a00ee1ca061e8a4bb297e4
HwangN <nhwa@umich.edu>
2015-11-11 01:59:43 -0500

    points

---
+++ BaseSmashConcept/Assets/__Scripts/Points.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour {
+	
+	static public int[] score;
+	public int id;
+	Text label;
+
+	static public void givePoints(int pid){
+		score[pid] += 100;
+	}
+
+	// Use this for initialization
+	void Start () {
+		score = new int[2];
+		score [0] = 0;
+		score [1] = 0;
+
+		label = GetComponent<Text> ();
+		showText();
+	}
+
+	void showText(){
+		label.text = "" + score[id];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		showText();
+	}
+
+}
