1391cc789157d2b950c553f9016eeb2890d775be
Haizhou Zhao <zhaohaizhou940527@gmail.com>
2015-11-11 21:51:50 -0500

    ThirdPersonCamera Preliminary Impl

--- BaseSmashConcept/Assets/__Scripts/Camera/ThirdPersonCamera.cs
+++ BaseSmashConcept/Assets/__Scripts/Camera/ThirdPersonCamera.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ThirdPersonCamera : MonoBehaviour {
+public class ThirdPersonCamera : MonoBehaviour
+{
+
+	#region private variables
+
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform follow;
+	private Vector3 targetPosition;
+
+	#endregion
+
+	#region public variables
+	public GameObject player;
+	#endregion
+
+	#region unity event functions
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		follow = player.transform;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+
 	}
+
+	void LateUpdate()
+	{
+		// calculate target position
+		targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+		// using linear interpolation to smooth out the camera movement
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+		// adjust camera direction
+		transform.LookAt(follow);
+	}
+
+	#endregion
 }
