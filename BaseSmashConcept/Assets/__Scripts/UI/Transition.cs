becededdf8ca7006feaaa128969c489091a41e3d
HwangN <nhwa@umich.edu>
2015-11-16 01:58:10 -0500

    menu stuff

---
+++ BaseSmashConcept/Assets/__Scripts/UI/Transition.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Transition : MonoBehaviour {
+
+	public Canvas controlScreen;
+	public Canvas titleScreen;
+	public GameObject view;
+	public GameObject back;
+	public EventSystem e;
+
+	void Start(){
+		titleScreen.enabled = true;
+		controlScreen.enabled = false;
+	}
+
+	public void StartGame(){
+		Application.LoadLevel ("_Scene_Main");
+	}
+
+	public void Controls(){
+		titleScreen.enabled = false;
+		controlScreen.enabled = true;
+		e.SetSelectedGameObject (back);
+	}
+
+	public void ControlsClose(){
+		titleScreen.enabled = true;
+		controlScreen.enabled = false;
+		e.SetSelectedGameObject (view);
+	}
+	
+}
