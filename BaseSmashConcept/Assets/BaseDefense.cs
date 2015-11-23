9f88238df923aff57cc9668f0b9393fd06e8ea33
joshviss <joshviss@umich.edu>
2015-11-23 05:41:42 -0500

    Can destroy gates.

---
+++ BaseSmashConcept/Assets/BaseDefense.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseDefense : MonoBehaviour {
+
+	public int health = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		GameObject collidedWith = other.gameObject;
+
+		if(collidedWith.tag == "PlayerAttackRange") {
+			if((collidedWith.layer + 6) != this.gameObject.layer) {
+				health -= 1;
+			}
+		}
+		if(health == 0) {
+			this.gameObject.SetActive(false);
+			health = 3;
+		}
+	}
+
+}
