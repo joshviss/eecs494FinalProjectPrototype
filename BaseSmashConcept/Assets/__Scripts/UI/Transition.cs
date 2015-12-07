33ffc478cd21b490af4a111d19f6ffd7a330d6e0
HwangN <nhwa@umich.edu>
2015-12-06 20:25:56 -0500

    Updated the title

--- BaseSmashConcept/Assets/__Scripts/UI/Transition.cs
+++ BaseSmashConcept/Assets/__Scripts/UI/Transition.cs
@@ -12,11 +12,14 @@ public class Transition : MonoBehaviour {
 	public GameObject back;
 	public GameObject back2;
 	public GameObject rule;
+	public GameObject BG;
 	public EventSystem e;
 
 	void Start(){
+		BG.SetActive (true);
 		titleScreen.enabled = true;
 		controlScreen.enabled = false;
+		ruleScreen.enabled = false;
 	}
 
 	public void StartGame(){
@@ -24,24 +27,28 @@ public class Transition : MonoBehaviour {
 	}
 
 	public void Controls(){
+		BG.SetActive (false);
 		titleScreen.enabled = false;
 		controlScreen.enabled = true;
 		e.SetSelectedGameObject (back);
 	}
 
 	public void ControlsClose(){
+		BG.SetActive (true);
 		titleScreen.enabled = true;
 		controlScreen.enabled = false;
 		e.SetSelectedGameObject (view);
 	}
 
 	public void Rules(){
+		BG.SetActive (false);
 		titleScreen.enabled = false;
 		ruleScreen.enabled = true;
 		e.SetSelectedGameObject (back2);
 	}
 
 	public void RulesClose(){
+		BG.SetActive (true);
 		titleScreen.enabled = true;
 		ruleScreen.enabled = false;
 		e.SetSelectedGameObject (rule);
