233fbb6eed5af64c69e81bc071a619c5e55e2e2b
Haizhou Zhao <zhaohaizhou940527@gmail.com>
2015-12-09 01:02:13 -0500

    My life for slow bomb

--- BaseSmashConcept/Assets/__Scripts/PlayerAbilities/SlowBomb.cs
+++ BaseSmashConcept/Assets/__Scripts/PlayerAbilities/SlowBomb.cs
@@ -1,29 +1,37 @@
-﻿/*using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class SlowBomb : MonoBehaviour {
-
-	private float initImpulse = 100.0f;
-	private Vector3 initDir = Vector3(2, 1, 0).normalized;
-	private float aoeRange = 4.0f;
+	
+	private float initImpulse = 10.0f;
+	private float aoeRange = 6.0f;
 	private float damage = 5.0f;
-	private float slowTime = 3.0f;
 	private Transform owner;
-
+	
 	public void init(Transform _owner)
 	{
 		owner = _owner;
-		initDir = owner.TransformDirection(initDir);
-		initDir = initDir.normalized;
-		GetComponent<Rigidbody>().AddForce(initDir * initImpulse, ForceMode.Impulse);
+		transform.position = owner.position;
+		transform.rotation = owner.rotation;
+		transform.position = transform.position + transform.forward;
+		GetComponent<Rigidbody>().AddForce(Quaternion.AngleAxis(-45, transform.right) * transform.forward * initImpulse, ForceMode.Impulse);
 	}
-
+	
 	void OnCollisionEnter(Collision coll)
 	{
-
+		GameObject collideWith = coll.gameObject;
+		if(collideWith.layer == LayerMask.NameToLayer("Ground") ||
+		   collideWith.layer == LayerMask.NameToLayer("Player1") ||
+		   collideWith.layer == LayerMask.NameToLayer("Player2") ||
+		   collideWith.layer == LayerMask.NameToLayer("Player3") ||
+		   collideWith.layer == LayerMask.NameToLayer("Player4"))
+		{
+			explode(transform.position, aoeRange, damage);
+			Destroy (gameObject);
+		}
 	}
-
-	void AreaDamageEnemies(Vector3 location, float radius, float damage)
+	
+	void explode(Vector3 location, float radius, float damage)
 	{
 		Collider[] objectsInRange = Physics.OverlapSphere(location, radius);
 		foreach (Collider col in objectsInRange)
@@ -38,4 +46,5 @@ public class SlowBomb : MonoBehaviour {
 				opponent.slow(effect);
 			}
 		}
-}*/
+	}
+}
